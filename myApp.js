da41824ec57c00137c8880948dd86cf03c7c9faf
seanEidrefNatividad <seaneidrefnatividad@gmail.com>
2024-12-04 15:56:53 +0800

    feat: add middleware

--- myApp.js
+++ myApp.js
@@ -1,6 +1,7 @@
 let express = require('express');
 let app = express();
 console.log("Hello World")
+app.use('/public',express.static(__dirname+'/public'))
 app.get('/', (req,res) => {
   res.sendFile(__dirname + '/views/index.html')
 })
