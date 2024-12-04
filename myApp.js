f0fbd17bb7dd70a4a7f114c18e9f399592057196
seanEidrefNatividad <seaneidrefnatividad@gmail.com>
2024-12-04 15:36:27 +0800

    feat: add sendFile /views/index.html

--- myApp.js
+++ myApp.js
@@ -2,7 +2,7 @@ let express = require('express');
 let app = express();
 console.log("Hello World")
 app.get('/', (req,res) => {
-  res.send('Hello Express')
+  res.sendFile(__dirname + '/views/index.html')
 })
 
 
